1cbae569fcf6e2412e7d5c234dd663cbe5e3b4b2
manju1399 <122274368+manju1399@users.noreply.github.com>
2023-01-16 18:22:12 +0530

    a
    a

---
+++ dark.js
@@ -0,0 +1,29 @@
+
+        let style=document.getElementById('s')
+        let x=document.getElementById('z')
+
+        
+        // let z=document.getElementById('x')
+        // console.log(x);
+        let form=document.getElementById('mfrm')
+        
+        form.addEventListener('click',dark);
+
+       function dark(q){
+
+           q.preventDefault();
+
+           if (x.className=='bi bi-moon') {
+                // console.log('running');
+                style.href="./style1.css"
+                x.className='bi bi-sun';
+                // x.style.color="rgb(119, 107, 224)"
+            } 
+            else if(x.className=='bi bi-sun') {
+                // console.log('not running');
+                style.href="./style.css"
+                x.className='bi bi-moon';
+                // x.style.color="rgb(119, 107, 224)"
+            }
+        }
+       
